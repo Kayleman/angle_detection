b5908ca20a8b07f7c61358f7eaa52ade5f95fbc8
Kayle <seiulaomi@126.com>
2018-01-04 16:48:45 +0800

    add watchdog

--- watchdog_img.py
+++ watchdog_img.py
@@ -2,14 +2,25 @@ import sys
 import time
 import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+
+
+class CreatedEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        FileSystemEventHandler.__init__(self)
+
+    def on_created(self, event):
+        file_name = event.src_path
+        print(file_name)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    # path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = "images/"
+    event_handler = CreatedEventHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
